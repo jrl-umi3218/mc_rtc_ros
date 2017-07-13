5037b0a8cca1ceada319916068130df1232f4c23
Hervé Audren <h.audren@gmail.com>
2017-07-13 17:36:48 +0900

    Update contact_marker

--- mc_sequence_visualization/src/mc_sequence_visualization/contact_marker.py
+++ mc_sequence_visualization/src/mc_sequence_visualization/contact_marker.py
@@ -13,13 +13,22 @@ class ContactControlMarker(object):
     self.server = InteractiveMarkerServer('%s_marker' % name)
     self.server.applyChanges()
 
-  def init(self, position):
+  def init(self, position, orientation=None, dof=None):
+    self.dof = dof
+    if dof is not None:
+      dim = len([d for d in self.dof if d == 0])
+    else:
+      dim = 6
+
     self.intMarker = InteractiveMarker()
     self.intMarker.header.frame_id = '/robot_map'
     self.intMarker.name = self.name
-    self.intMarker.description = 'Contact 3d control'
+    #FIXME : This only works for 3D and 6D
+    self.intMarker.description = '{} {}D control'.format(self.name, dim)
     self.intMarker.pose.position = Point(*position)
-    self.intMarker.scale = 1.
+    if orientation is not None:
+      quat = list(Quaterniond(orientation).inverse().coeffs())
+      self.intMarker.pose.orientation = Quaternion(*quat)
 
     self.marker = Marker()
     self.marker.type = Marker.CUBE
@@ -40,17 +49,42 @@ class ContactControlMarker(object):
       transControl.orientation = Quaternion(1., axis[0], axis[1], axis[2])
       return transControl
 
+    def mkRotControl(name, axis):
+      transControl = InteractiveMarkerControl()
+      transControl.name = name
+      transControl.interaction_mode = InteractiveMarkerControl.ROTATE_AXIS
+      transControl.orientation = Quaternion(1., axis[0], axis[1], axis[2])
+      return transControl
+
     self.transXControl = mkTransControl('trans_x', (1., 0., 0.))
     self.transYControl = mkTransControl('trans_y', (0., 1., 0.))
     self.transZControl = mkTransControl('trans_z', (0., 0., 1.))
 
+    self.rotXControl = mkRotControl('rot_x', (1., 0., 0.))
+    self.rotYControl = mkRotControl('rot_y', (0., 1., 0.))
+    self.rotZControl = mkRotControl('rot_z', (0., 0., 1.))
+
+    #FIXME : This maps dof to controls, but it is weird.
+    #Works for drc_stairs and drc_egress
+    self.controls = [self.rotZControl,
+                     self.rotXControl,
+                     self.rotYControl,
+                     self.transZControl,
+                     self.transXControl,
+                     self.transYControl]
+
     self.intMarker.controls.append(self.visControl)
-    self.intMarker.controls.append(self.transXControl)
-    self.intMarker.controls.append(self.transYControl)
-    self.intMarker.controls.append(self.transZControl)
+
+    if self.dof is None:
+      self.intMarker.controls.extend(self.controls)
+    else:
+      print self.name, self.dof
+      ctrl = [c for c, f in zip(self.controls, self.dof) if f == 0]
+      self.intMarker.controls.extend(ctrl)
 
     self.server.insert(self.intMarker, self.callback)
     self.server.applyChanges()
+
 
   def clear(self):
     self.server.erase(self.intMarker.name)
