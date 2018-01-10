755ee59a14036f0697b33c1047ee9c2111211e73
Pierre Gergondet <pierre.gergondet@gmail.com>
2018-01-10 18:58:09 +0100

    Improve Panel main code clarity

--- mc_rtc_rviz_panel/src/Panel.cpp
+++ mc_rtc_rviz_panel/src/Panel.cpp
@@ -1,22 +1,89 @@
 #include "Panel.h"
+
+#include "ButtonWidget.h"
+#include "FormWidget.h"
 #include "InputWidget.h"
 #include "LabelWidget.h"
 #include "Point3DWidget.h"
+#include "SchemaWidget.h"
 
 #include <mc_rtc/Configuration.h>
+
+#include <stack>
+
+namespace
+{
+  constexpr int MAX_TAB_LEVEL = 2;
+  static_assert(MAX_TAB_LEVEL >= 0, "Must have at least zero tab level");
+
+  struct FoldableWidget : public QWidget
+  {
+    FoldableWidget(const std::string & name, bool always_show)
+    {
+      auto layout = new QVBoxLayout();
+      setLayout(layout);
+      topLayout = new QVBoxLayout();
+      bottomLayout = new QVBoxLayout();
+      layout->addLayout(topLayout);
+      layout->addLayout(bottomLayout);
+      if(!always_show)
+      {
+        showButton = new QPushButton(name.c_str());
+        showButton->setCheckable(true);
+        topLayout->addWidget(showButton);
+        connect(showButton, &QPushButton::toggled,
+            this, [this](bool)
+            {
+            for(const auto & w : widgets_)
+            {
+              if(showButton->isChecked()) { w->show(); }
+              else { w->hide(); }
+            }
+            });
+      }
+    }
+    void addWidget(QWidget * w, bool is_foldable)
+    {
+      widgets_.push_back(w);
+      if(showButton && !showButton->isChecked())
+      {
+        w->hide();
+      }
+      else
+      {
+        w->show();
+      }
+      if(is_foldable) { bottomLayout->addWidget(w); }
+      else { topLayout->addWidget(w); }
+    }
+    void removeWidget(QWidget * w, bool is_foldable)
+    {
+      auto it = std::find(widgets_.begin(), widgets_.end(), w);
+      if(is_foldable) { bottomLayout->removeWidget(w); }
+      else { topLayout->removeWidget(w); }
+      if(it != widgets_.end())
+      {
+        widgets_.erase(it);
+      }
+    }
+    QVBoxLayout * topLayout = nullptr;
+    QVBoxLayout * bottomLayout = nullptr;
+    QPushButton * showButton = nullptr;
+    std::vector<QWidget*> widgets_ = {};
+  };
+}
 
 namespace mc_rtc_rviz
 {
 
 Panel::Panel(QWidget * parent)
-: rviz::Panel(parent),
-  mc_control::ControllerClient("ipc:///tmp/mc_rtc_pub.ipc", "ipc:///tmp/mc_rtc_rep.ipc"),
-  tabW_(parent),
+: QWidget(parent),
+  //FIXME Hard-coded parameters for the client
+  mc_control::ControllerClient("ipc:///tmp/mc_rtc_pub.ipc", "ipc:///tmp/mc_rtc_rep.ipc", 10.),
   nh_(),
-  int_server_("mc_rtc_rviz_interactive_markers")
+  int_server_(std::make_shared<interactive_markers::InteractiveMarkerServer>("mc_rtc_rviz_interactive_markers"))
 {
-  auto mainLayout = new QVBoxLayout();
-  mainLayout->addWidget(&tabW_);
+  mainLayout = new QVBoxLayout();
   setLayout(mainLayout);
   connect(this, SIGNAL(gotData(const char*)),
           this, SLOT(handle_gui_state_impl(const char*)));
@@ -24,16 +91,25 @@ Panel::Panel(QWidget * parent)
 
 namespace
 {
-  BaseWidget * makeEntry(const std::vector<std::string> & category,
+  BaseWidget * makeEntry(QWidget * parent,
+                         const std::vector<std::string> & category,
                          const std::string & name,
                          const mc_rtc::Configuration & data,
                          mc_control::ControllerClient & client,
-                         interactive_markers::InteractiveMarkerServer & int_server)
+                         std::shared_ptr<interactive_markers::InteractiveMarkerServer> int_server)
   {
     auto gui_type = data("GUI")("type");
     if(gui_type == "Label")
     {
       return new LabelWidget(name, data);
+    }
+    if(gui_type == "Button")
+    {
+      return new ButtonWidget(name,
+        [&client,category,name](const mc_rtc::Configuration & data)
+        {
+          client.send_request(category, name, data);
+        });
     }
     if(gui_type == "Point3D")
     {
@@ -52,65 +128,24 @@ namespace
             client.send_request(category, name, data);
           });
     }
+    if(gui_type == "Schema")
+    {
+      return new SchemaWidget(parent, data,
+          [&client,category,name](const mc_rtc::Configuration & data)
+          {
+            client.send_request(category, name, data);
+          });
+    }
+    if(gui_type == "Form")
+    {
+      return new FormWidget(parent, data,
+          [&client,category,name](const mc_rtc::Configuration & data)
+          {
+            client.send_request(category, name, data);
+          });
+    }
     std::cerr << "Cannot handle the provided GUI type " << gui_type << std::endl;
     return nullptr;
-  }
-
-  QWidget * makeCategoryWidget()
-  {
-    auto ret = new QWidget();
-    ret->setLayout(new QVBoxLayout());
-    return ret;
-  }
-
-  QWidget * makeGroupWidget(const std::string & name,
-                            QWidget * parent)
-  {
-    auto gb = new QGroupBox(name.c_str(), parent);
-    gb->setLayout(new QVBoxLayout());
-    parent->layout()->addWidget(gb);
-    return gb;
-  }
-
-  void updateCategoryWidget(QWidget * cat,
-      const std::string & cat_name,
-      std::map<std::string, QWidget *> & cat_widgets,
-      const std::map<std::string, mc_rtc::Configuration> & entries,
-      mc_control::ControllerClient & client,
-      interactive_markers::InteractiveMarkerServer & int_server)
-  {
-    for(const auto & e : entries)
-    {
-      const auto & e_name = e.first;
-      std::map<std::string, mc_rtc::Configuration> data = e.second;
-      if(!cat_widgets.count(e_name))
-      {
-        cat_widgets[e_name] = makeGroupWidget(e_name, cat);
-      }
-      auto group_w = cat_widgets[e_name];
-      for(const auto & d : data)
-      {
-        std::string entry = e_name + "/" + d.first;
-        if(!cat_widgets.count(entry))
-        {
-          auto w = makeEntry({cat_name, e_name}, d.first, d.second, client, int_server);
-          cat_widgets[entry] = w;
-          if(w)
-          {
-            //group_w->layout()->addWidget(w);
-            static_cast<QVBoxLayout*>(group_w->layout())->addLayout(w->layout);
-          }
-        }
-        auto w = cat_widgets[entry];
-        if(w)
-        {
-          if(d.second.has("data"))
-          {
-            static_cast<BaseWidget*>(w)->update(d.second("data"));
-          }
-        }
-      }
-    }
   }
 }
 
@@ -119,25 +154,158 @@ void Panel::handle_gui_state(const char * data_raw, size_t size)
   Q_EMIT gotData(data_raw);
 }
 
+void Panel::handle_category(const std::string & category,
+                            const std::string & name,
+                            const std::map<std::string, mc_rtc::Configuration> & items,
+                            const mc_rtc::Configuration & data,
+                            QWidget * parent,
+                            int level,
+                            std::vector<std::string> & seen)
+{
+  if(items.size() == 0) { return; }
+  if(items.count("GUI"))
+  {
+    /** Actual item */
+    if(!widgets_.count(category))
+    {
+      auto q_categories = QString(category.c_str()).split("/");
+      std::vector<std::string> categories {};
+      for(int i = 1; i < q_categories.count() - 1; ++i)
+      {
+        const auto & c = q_categories[i];
+        categories.emplace_back(c.toStdString());
+      }
+      auto w = makeEntry(parent, categories, name, data, *this, int_server_);
+      widgets_[category] = w;
+      if(w)
+      {
+        if(level <= MAX_TAB_LEVEL)
+        {
+          parent->layout()->addWidget(w);
+          remove_w_[category] = [parent](QWidget * w)
+          {
+            parent->layout()->removeWidget(w);
+          };
+        }
+        else
+        {
+          static_cast<FoldableWidget*>(parent)->addWidget(w, false);
+          remove_w_[category] = [parent](QWidget * w)
+          {
+            static_cast<FoldableWidget*>(parent)->removeWidget(w, false);
+          };
+        }
+      }
+    }
+    seen.push_back(category);
+    auto w = static_cast<BaseWidget*>(widgets_[category]);
+    if(w && items.count("data"))
+    {
+      w->update(items.at("data"));
+    }
+  }
+  else
+  {
+    /** Category */
+    if(!widgets_.count(category))
+    {
+      QWidget * w = nullptr;
+      if(level < MAX_TAB_LEVEL)
+      {
+        w = new QTabWidget();
+        w->setLayout(new QVBoxLayout());
+      }
+      else
+      {
+        w = new FoldableWidget(name, level == MAX_TAB_LEVEL);
+      }
+      if(level == 0)
+      {
+        assert(parent == this);
+        this->mainLayout->addWidget(w);
+        remove_w_[category] = [this](QWidget * w)
+        {
+          this->mainLayout->removeWidget(w);
+        };
+      }
+      else if(level <= MAX_TAB_LEVEL)
+      {
+        auto p = static_cast<QTabWidget*>(parent);
+        p->addTab(w, name.c_str());
+        remove_w_[category] = [p](QWidget * w)
+        {
+          p->removeTab(p->indexOf(w));
+        };
+      }
+      else
+      {
+        auto p = static_cast<FoldableWidget*>(parent);
+        p->addWidget(w, true);
+        remove_w_[category] = [p](QWidget * w)
+        {
+          p->removeWidget(w, true);
+        };
+      }
+      widgets_[category] = w;
+    }
+    seen.push_back(category);
+    auto w = widgets_[category];
+    for(const auto & i : items)
+    {
+      handle_category(category + "/" + i.first,
+                      i.first,
+                      i.second,
+                      i.second,
+                      w,
+                      level + 1, seen);
+    }
+  }
+}
+
 void Panel::handle_gui_state_impl(const char * data_raw)
 {
-  std::map<std::string, mc_rtc::Configuration> data = mc_rtc::Configuration::fromData(data_raw);
-  for(const auto & c : data)
+  auto data = mc_rtc::Configuration::fromData(data_raw);
+  std::vector<std::string> seen {};
+  handle_category("", "", data, data, this, 0, seen);
+  /** Handle removed elements */
+  std::stack<QWidget*> to_delete;
+  for(auto w_iter = widgets_.begin(); w_iter != widgets_.end();)
   {
-    const auto & category = c.first;
-    const auto & c_data = c.second;
-    if(!tabs_.count(category))
+    const auto & w = *w_iter;
+    if(std::find(seen.begin(), seen.end(), w.first) == seen.end())
     {
-      tabs_[category] = makeCategoryWidget();
-      tabs_index_[category] = tabW_.addTab(tabs_[category], category.c_str());
+      if(w.second && remove_w_.count(w.first))
+      {
+        remove_w_[w.first](w.second);
+        remove_w_.erase(w.first);
+      }
+      to_delete.push(w.second);
+      w_iter = widgets_.erase(w_iter);
     }
-    updateCategoryWidget(tabs_[category], category, widgets_[category], c_data, *this, int_server_);
+    else
+    {
+      ++w_iter;
+    }
   }
-  /** FIXME Handle disappearance of some tabs */
-  int_server_.applyChanges();
+  while(to_delete.size())
+  {
+    auto w = to_delete.top();
+    delete w;
+    to_delete.pop();
+  }
+  int_server_->applyChanges();
+}
+
+MyPanel::MyPanel(QWidget * parent)
+: rviz::Panel(parent)
+{
+  auto layout = new QVBoxLayout();
+  panel = new mc_rtc_rviz::Panel(parent);
+  layout->QLayout::addWidget(panel);
+  setLayout(layout);
 }
 
 }
 
 #include <pluginlib/class_list_macros.h>
-PLUGINLIB_EXPORT_CLASS(mc_rtc_rviz::Panel, rviz::Panel)
+PLUGINLIB_EXPORT_CLASS(mc_rtc_rviz::MyPanel, rviz::Panel)
--- mc_rtc_rviz_panel/src/Panel.h
+++ mc_rtc_rviz_panel/src/Panel.h
@@ -26,25 +26,41 @@
 namespace mc_rtc_rviz
 {
 
-class Panel: public rviz::Panel, public mc_control::ControllerClient
+class Panel: public QWidget, public mc_control::ControllerClient
 {
 Q_OBJECT
 public:
   Panel(QWidget* parent = 0);
 private:
-  void handle_gui_state(const char * data, size_t size);
-  /** GUI stuff */
-  QTabWidget tabW_;
-  std::map<std::string, QWidget*> tabs_;
-  std::map<std::string, int> tabs_index_;
-  std::map<std::string, std::map<std::string, QWidget*>> widgets_;
+  void handle_gui_state(const char * data, size_t size) override;
   /** ROS stuff */
   ros::NodeHandle nh_;
-  interactive_markers::InteractiveMarkerServer int_server_;
+  std::shared_ptr<interactive_markers::InteractiveMarkerServer> int_server_;
+
+  /** GUI stuff */
+  QLayout * mainLayout;
+  std::map<std::string, QWidget*> widgets_;
+  std::map<std::string, std::function<void(QWidget*)>> remove_w_;
+  void handle_category(const std::string & category,
+                       const std::string & name,
+                       const std::map<std::string, mc_rtc::Configuration> & items,
+                       const mc_rtc::Configuration & data,
+                       QWidget * parent,
+                       int level,
+                       std::vector<std::string> & seen);
 private slots:
   void handle_gui_state_impl(const char * data);
 signals:
   void gotData(const char * data);
+};
+
+class MyPanel : public rviz::Panel
+{
+Q_OBJECT
+public:
+  MyPanel(QWidget * parent = 0);
+
+  mc_rtc_rviz::Panel * panel;
 };
 
 }
