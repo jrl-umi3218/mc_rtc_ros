b684c193d3166ad9c70aa270825ef44a9e6d0783
Pierre Gergondet <pierre.gergondet@gmail.com>
2018-01-22 23:26:10 +0100

    Make FormWidget a bit more usable

--- mc_rtc_rviz_panel/src/FormWidget.cpp
+++ mc_rtc_rviz_panel/src/FormWidget.cpp
@@ -13,9 +13,44 @@ FormWidget::FormWidget(QWidget * parent, const mc_rtc::Configuration & data,
   auto elements = data("GUI")("elements", std::map<std::string, mc_rtc::Configuration>{});
   for(const auto & el : elements)
   {
-    layout_->addRow(el.first.c_str(), new QLineEdit());
+    auto w = new QLineEdit();
+    auto name = el.first;
+    layout_->addRow(el.first.c_str(), w);
+    element_callbacks_.push_back(
+      [name,w](mc_rtc::Configuration & out)
+      {
+      bool ok = false;
+        unsigned int uivalue = w->text().toUInt(&ok);
+        if(ok)
+        {
+          out.add(name, uivalue);
+        }
+        else
+        {
+          double dvalue = w->text().toDouble(&ok);
+          if(ok)
+          {
+            out.add(name, dvalue);
+          }
+          else
+          {
+            out.add(name, w->text().toStdString());
+          }
+        }
+      }
+    );
   }
   layout_->addRow(confirm_button_);
+  confirm_button_->connect(confirm_button_, &QPushButton::released,
+                           this, [this]()
+                           {
+                           mc_rtc::Configuration req;
+                           for(const auto & el : element_callbacks_)
+                           {
+                            el(req);
+                           }
+                           request_(req);
+                           });
   setLayout(layout);
 }
 
--- mc_rtc_rviz_panel/src/FormWidget.h
+++ mc_rtc_rviz_panel/src/FormWidget.h
@@ -18,6 +18,8 @@ struct FormWidget : public BaseWidget
   void update(const mc_rtc::Configuration & data) override final {}
 private:
   request_t request_;
+  using element_callback_t = std::function<void(mc_rtc::Configuration&)>;
+  std::vector<element_callback_t> element_callbacks_;
   QFormLayout * layout_;
   QPushButton * confirm_button_ = new QPushButton("Confirm");
 };
