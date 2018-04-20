9f0c1f0c76824c3dc5633bbb56ce7771a7194ed5
Pierre Gergondet <pierre.gergondet@gmail.com>
2018-04-20 19:12:39 +0900

    Small display improvment for ArrayLabelWidget

--- mc_rtc_rviz_panel/src/ArrayLabelWidget.cpp
+++ mc_rtc_rviz_panel/src/ArrayLabelWidget.cpp
@@ -12,21 +12,38 @@ ArrayLabelWidget::ArrayLabelWidget(const ClientWidgetParam & param,
 
 void ArrayLabelWidget::update(const Eigen::VectorXd & v)
 {
-  label_->setText(QString::number(v.norm()));
-  std::stringstream tooltip;
-  for(size_t i = 0; i < v.size(); ++i)
+  if(labels_.size() == 0)
   {
-    if(i < labels_.size())
+    label_->setText(QString::number(v.norm()));
+    std::stringstream tooltip;
+    for(size_t i = 0; i < v.size(); ++i)
     {
-      tooltip << labels_[i] << ": ";
+      tooltip << v(i);
+      if(i < v.size() - 1)
+      {
+        tooltip << ", ";
+      }
     }
-    tooltip << v(i);
-    if(i < v.size() - 1)
-    {
-      tooltip << ", ";
-    }
+    label_->setToolTip(tooltip.str().c_str());
   }
-  label_->setToolTip(tooltip.str().c_str());
+  else
+  {
+    std::stringstream text;
+    for(size_t i = 0; i < v.size(); ++i)
+    {
+      if(i < labels_.size())
+      {
+        text << labels_[i] << ": ";
+      }
+      text << v(i);
+      if(i < v.size() - 1)
+      {
+        text << ", ";
+      }
+    }
+    label_->setText(text.str().c_str());
+    label_->setToolTip(QString::number(v.norm()));
+  }
 }
 
 }
