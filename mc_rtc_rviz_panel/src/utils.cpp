4ee74c6f6748671ecdc6c372dab6d5fa95a53833
Pierre Gergondet <pierre.gergondet@gmail.com>
2018-12-10 14:30:10 +0900

    Clean up SharedMarker code

--- mc_rtc_rviz_panel/src/utils.cpp
+++ mc_rtc_rviz_panel/src/utils.cpp
@@ -3,6 +3,9 @@
 namespace mc_rtc_rviz
 {
 
+namespace
+{
+
 vm::Marker makeVisual(int t, const vm::InteractiveMarker & marker)
 {
   vm::Marker ret;
@@ -94,6 +97,71 @@ vm::InteractiveMarker make6DMarker(const std::string & name,
   }
 
   return ret;
+}
+
+}
+
+SharedMarker::SharedMarker(interactive_markers::InteractiveMarkerServer & server,
+                           const std::string & name,
+                           bool control_position, bool control_orientation, int type,
+                           interactive_markers::InteractiveMarkerServer::FeedbackCallback callback)
+: server_(server),
+  marker_(make6DMarker(name, control_position, control_orientation, type)),
+  callback_(callback)
+{
+  server_.insert(marker_, callback_);
+}
+
+SharedMarker::~SharedMarker()
+{
+  if(!hidden_)
+  {
+    server_.erase(marker_.name);
+  }
+}
+
+void SharedMarker::toggle()
+{
+  if(hidden_)
+  {
+    hidden_ = false;
+    server_.insert(marker_, callback_);
+  }
+  else
+  {
+    hidden_ = true;
+    server_.erase(marker_.name);
+  }
+}
+
+void SharedMarker::update(const Eigen::Vector3d & t)
+{
+  if(!hidden_)
+  {
+    geometry_msgs::Pose pose;
+    pose.orientation.w = 1.0;
+    pose.position.x = t.x();
+    pose.position.y = t.y();
+    pose.position.z = t.z();
+    server_.setPose(marker_.name, pose);
+  }
+}
+
+void SharedMarker::update(const sva::PTransformd & pos)
+{
+  if(!hidden_)
+  {
+    geometry_msgs::Pose pose;
+    Eigen::Quaterniond q {pos.rotation().transpose()};
+    pose.orientation.w = q.w();
+    pose.orientation.x = q.x();
+    pose.orientation.y = q.y();
+    pose.orientation.z = q.z();
+    pose.position.x = pos.translation().x();
+    pose.position.y = pos.translation().y();
+    pose.position.z = pos.translation().z();
+    server_.setPose(marker_.name, pose);
+  }
 }
 
 }
--- mc_rtc_rviz_panel/src/utils.h
+++ mc_rtc_rviz_panel/src/utils.h
@@ -11,78 +11,20 @@ namespace vm = visualization_msgs;
 namespace mc_rtc_rviz
 {
 
-vm::Marker makeVisual(int t, const vm::InteractiveMarker & marker);
-
-vm::InteractiveMarkerControl & makeVisualControl(int t,
-                                                 vm::InteractiveMarker & marker);
-
-vm::InteractiveMarker make6DMarker(const std::string & name,
-                                   bool control_position,
-                                   bool control_orientation,
-                                   int type);
-
 struct SharedMarker
 {
   SharedMarker(interactive_markers::InteractiveMarkerServer & server,
-               const visualization_msgs::InteractiveMarker & marker,
-               interactive_markers::InteractiveMarkerServer::FeedbackCallback callback)
-  : server_(server),
-    marker_(marker),
-    callback_(callback)
-  {
-    server_.insert(marker, callback);
-  }
-  ~SharedMarker()
-  {
-    if(!hidden_)
-    {
-      server_.erase(marker_.name);
-    }
-  }
+               const std::string & name,
+               bool control_position, bool control_orientation, int type,
+               interactive_markers::InteractiveMarkerServer::FeedbackCallback callback);
 
-  void toggle()
-  {
-    if(hidden_)
-    {
-      hidden_ = false;
-      server_.insert(marker_, callback_);
-    }
-    else
-    {
-      hidden_ = true;
-      server_.erase(marker_.name);
-    }
-  }
+  ~SharedMarker();
 
-  void update(const Eigen::Vector3d & t)
-  {
-    if(!hidden_)
-    {
-      geometry_msgs::Pose pose;
-      pose.orientation.w = 1.0;
-      pose.position.x = t.x();
-      pose.position.y = t.y();
-      pose.position.z = t.z();
-      server_.setPose(marker_.name, pose);
-    }
-  }
+  void toggle();
 
-  void update(const sva::PTransformd & pos)
-  {
-    if(!hidden_)
-    {
-      geometry_msgs::Pose pose;
-      Eigen::Quaterniond q {pos.rotation().transpose()};
-      pose.orientation.w = q.w();
-      pose.orientation.x = q.x();
-      pose.orientation.y = q.y();
-      pose.orientation.z = q.z();
-      pose.position.x = pos.translation().x();
-      pose.position.y = pos.translation().y();
-      pose.position.z = pos.translation().z();
-      server_.setPose(marker_.name, pose);
-    }
-  }
+  void update(const Eigen::Vector3d & t);
+
+  void update(const sva::PTransformd & pos);
 private:
   interactive_markers::InteractiveMarkerServer & server_;
   vm::InteractiveMarker marker_;
