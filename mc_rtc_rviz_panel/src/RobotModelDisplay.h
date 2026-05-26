1c415b68eecddb6e832719759c19d824f46fa748
Martin Filliung <martin.filliung@aist.go.jp>
2026-05-26 17:25:20 +0900

    fix: change fix from 7a8fb209d23c, moved the definition inside the namespace

--- mc_rtc_rviz_panel/src/RobotModelDisplay.h
+++ mc_rtc_rviz_panel/src/RobotModelDisplay.h
@@ -6,17 +6,17 @@
 
 #include "ClientWidget.h"
 
+namespace mc_rtc_rviz
+{
 #ifdef MC_RTC_ROS_IS_ROS2
-using RVizDisplay = rviz_common::Display;
+using Display = rviz_common::Display;
 const std::string display_class_id = "rviz_default_plugins/RobotModel";
 const std::string description_prop = "Description Topic";
 #else
-using RVizDisplay = rviz::Display;
+using Display = rviz::Display;
 const std::string display_class_id = "rviz/RobotModel";
 const std::string description_prop = "Robot Description";
 #endif
-namespace mc_rtc_rviz
-{
 /** Handle label and display mangement of Robot */
 struct RobotModelDisplay : public ClientWidget
 {
@@ -27,6 +27,6 @@ struct RobotModelDisplay : public ClientWidget
 protected:
   QHBoxLayout * layout_;
   QLabel * label_;
-  QPointer<RVizDisplay> robot_model_display = nullptr;
+  QPointer<Display> robot_model_display = nullptr;
 };
 } // namespace mc_rtc_rviz
